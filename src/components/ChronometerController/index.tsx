c0036b53e0bfad6d9d3633c589a8801bdcfdfe6d
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-28 20:40:53 -0300

    building substract logic

--- src/components/ChronometerController/index.tsx
+++ src/components/ChronometerController/index.tsx
@@ -1,21 +1,42 @@
 import { cardsInfo } from '../../lib/cardsInfo'
-import { exercises } from '../../lib/exercises'
 import { BreathingCard } from '../BreathingCard'
 import { BreathingCardType } from '../../types/BreathingCardType'
+import { ExercisesType } from '../../types/Exercises'
+import { useEffect } from 'react'
 
-export const ChronometerController = ({isChronometerRunning}:{isChronometerRunning:boolean}) => {
+export const ChronometerController = ({isChronometerRunning, selectedExercise}:{isChronometerRunning:boolean, selectedExercise:ExercisesType}) => {
+  
+  const setInitialCardsValue = () => {
+    for(let i = 0; i < cardsInfo.length; i ++){
+      if(i === 0) cardsInfo[i].second = selectedExercise.inspiration
+      if(i === 1) cardsInfo[i].second = selectedExercise.holdRespiration
+      if(i === 2) cardsInfo[i].second = selectedExercise.expiration
+      //if(i > 2) Set custom exercises on the future
+    }
+  }
+  setInitialCardsValue()
+  useEffect(() => {setInitialCardsValue()}, [selectedExercise])
   
   const handleSubtractSeconds = () => {
-    
     for(let i = 0; i < cardsInfo.length; i ++){
-      cardsInfo[i].second = exercises.defaultExercise.inspiration
-      setTimeout(() => {})
-    }
-    setInterval(() => {
+      let currentCardSecond = 0
 
+      if(i === 0) currentCardSecond = selectedExercise.inspiration
+      if(i === 1) currentCardSecond = selectedExercise.holdRespiration
+      if(i === 2) currentCardSecond = selectedExercise.expiration
+
+      for(let i2 = currentCardSecond; i2 > 0; i2--){
+        setTimeout(() => {
+          currentCardSecond-- 
+          cardsInfo[i].second = currentCardSecond
+          
+        }, 1000)
+        console.log(currentCardSecond)
+      }
     }
-    ,1000)
   }
+
+
   return (
     <>
     {
